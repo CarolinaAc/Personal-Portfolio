bec5ea6f351501c90192808d60f5007ffd0e9153
Carolina Acosta <carolinaacosta.n@gmail.com>
2020-03-10 13:11:58 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,41 @@
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll('.nav__link')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+
+
+
+
+/*slides */
+let slideIndex = 1;
+            
+const plusSlides = n => {showSlides(slideIndex += n)}
+
+const currentSlide = n => {showSlides(slideIndex = n)}
+
+const showSlides = n => {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+showSlides(slideIndex);
